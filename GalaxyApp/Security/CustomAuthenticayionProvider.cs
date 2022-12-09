12c7fdd25440ecf220c4a22a4f89cc10eb3ec5d1
tanya_rybina <tryb2516@gmail.com>
2022-12-09 02:07:04 +0200

    changes probably

--- GalaxyApp/Security/CustomAuthenticayionProvider.cs
+++ GalaxyApp/Security/CustomAuthenticayionProvider.cs
@@ -1,5 +1,6 @@
 ﻿using GalaxyApp.Helpers.Abstractions;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.Options;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 
@@ -7,11 +8,25 @@ namespace GalaxyApp.Security
 {
     public class CustomAuthenticayionProvider : AuthenticationStateProvider
     {
+        private const string EXPIRATION_CLAIM = "exp";
         private readonly IBrowserStorageHelper storageHelper;
 
-        public CustomAuthenticayionProvider(IBrowserStorageHelper storageHelper)
+        public CustomAuthenticayionProvider(
+            IBrowserStorageHelper storageHelper)
         {
             this.storageHelper = storageHelper;
+        }
+
+        private bool IsTokenExpired(JwtSecurityToken token)
+        {
+            var claim = token.Claims
+                .First(claim => claim.Type == EXPIRATION_CLAIM)
+                .Value;
+            var ticks = long.Parse(claim);
+            DateTime tokenExpDate = DateTimeOffset.FromUnixTimeSeconds(ticks).UtcDateTime;
+            DateTime now = DateTime.UtcNow;
+
+            return tokenExpDate >= now;
         }
 
         public override async Task<AuthenticationState> GetAuthenticationStateAsync()
@@ -20,6 +35,11 @@ namespace GalaxyApp.Security
             if (!string.IsNullOrEmpty(token))
             {
                 var jwtToken = new JwtSecurityToken(jwtEncodedString: token);
+                if (IsTokenExpired(jwtToken))
+                {
+                    await storageHelper.Logout();
+                    return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+                }
                 //var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(jwtToken.Claims));
                 var claimsIdentity = new ClaimsIdentity(jwtToken.Claims, "user");
                 return new AuthenticationState(new ClaimsPrincipal(claimsIdentity));
