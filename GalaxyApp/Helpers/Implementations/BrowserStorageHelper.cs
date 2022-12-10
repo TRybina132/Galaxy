16d7e21f36074cb4378063ffd909ed403bd0cc0c
tanya_rybina <tryb2516@gmail.com>
2022-12-10 15:38:02 +0200

    delete token if expired

--- GalaxyApp/Helpers/Implementations/BrowserStorageHelper.cs
+++ GalaxyApp/Helpers/Implementations/BrowserStorageHelper.cs
@@ -1,5 +1,6 @@
 ﻿using GalaxyApp.Helpers.Abstractions;
 using Microsoft.JSInterop;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace GalaxyApp.Helpers.Implementations
 {
@@ -13,12 +14,41 @@ namespace GalaxyApp.Helpers.Implementations
             this.jsRuntime = jsRuntime;
         }
 
+        private long GetTokenExp(string token)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            var jwtSecurityToken = handler.ReadJwtToken(token);
+            var tokenExp = jwtSecurityToken.Claims.First(claim => claim.Type.Equals("exp")).Value;
+            var ticks = long.Parse(tokenExp);
+            return ticks;
+        }
+
+        public async Task<bool> IsExpired(string token = null)
+        {
+            if(string.IsNullOrEmpty(token))
+                token = await jsRuntime.InvokeAsync<string>("localStorage.getItem", "user")
+                    .ConfigureAwait(false);
+
+            var tokenTicks = GetTokenExp(token);
+            var tokenExpDate = DateTimeOffset.FromUnixTimeSeconds(tokenTicks).UtcDateTime;
+
+            var now = DateTime.Now.ToUniversalTime();
+
+            bool isExpired = tokenExpDate >= now;
+            if (isExpired)
+                await Logout();
+            return isExpired;
+        }
+
         public async Task SetTokenToStorage(string token) =>
             await jsRuntime.InvokeVoidAsync("localStorage.setItem", "user", token)
                 .ConfigureAwait(false);
 
-        public async Task<bool> IsAuthenticated() =>
-            await jsRuntime.InvokeAsync<string>("localStorage.getItem", "user") != null;
+        public async Task<bool> IsAuthenticated()
+        {
+            string token = await GetTokenAsync();
+            return !string.IsNullOrEmpty(token) && !await IsExpired(token);
+        }
 
         public async Task Logout() =>
             await jsRuntime.InvokeVoidAsync("localStorage.removeItem", "user");
